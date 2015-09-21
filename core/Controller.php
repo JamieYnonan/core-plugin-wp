91375e5b6f19a85e932c6f173699003b854679fe
Jamie Ynonan <jamiea31@gmail.com>
2015-09-21 10:05:38 -0500

    Controller, add method isAjax

--- core/Controller.php
+++ core/Controller.php
@@ -38,4 +38,10 @@ abstract class Controller
 		}
 		return $post;
 	}
+
+	protected function isAjax()
+	{
+		return (!empty($_SERVER['HTTP_X_REQUESTED_WITH'])
+			&& strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest');
+	}
 }
